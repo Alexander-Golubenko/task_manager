272aac666c7bcde9417b768eabb7af90822ef363
Alexander-Golubenko <alexarcad@gmail.com>
2025-06-05 23:30:46 +0200

    Initial commit. Clean project without data

---
+++ models.py
@@ -0,0 +1,44 @@
+from django.db import models
+
+
+STATUS_CHOICES = [
+    ('N', 'New'),
+    ('IP', 'In Progress'),
+    ('D', 'Done'),
+    ('P', 'Pending'),
+    ('B', 'Blocked')
+]
+
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=50, verbose_name='Title', unique_for_date='deadline')
+    description = models.TextField(verbose_name='Description')
+    categories = models.ManyToManyField("Category", verbose_name='Categories')
+    status = models.CharField(choices=STATUS_CHOICES, default='N', max_length=15, verbose_name='Status')
+    deadline = models.DateField(verbose_name='Deadline')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Created at')
+
+    def __str__(self):
+        return f'{self.title}'
+
+
+class SubTask(models.Model):
+    title = models.CharField(max_length=50, verbose_name='Title')
+    description = models.TextField(verbose_name='Description')
+    task = models.ForeignKey(Task, on_delete=models.CASCADE, verbose_name='Task', related_name='subtasks')
+    status = models.CharField(choices=STATUS_CHOICES, default='N', max_length=15, verbose_name='Status')
+    deadline = models.DateField(verbose_name='Deadline')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Created at')
+
+    def __str__(self):
+        return f'{self.title}'
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Name')
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name_plural = 'Categories'
