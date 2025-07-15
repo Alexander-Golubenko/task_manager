33abbef0df9aa98634870078c1acfd154499b35e
Alexander-Golubenko <alexarcad@gmail.com>
2025-07-15 20:58:24 +0200

    hw_13. Added serializers && updated views

--- serializers.py
+++ serializers.py
@@ -1,5 +1,7 @@
+from django.utils.timezone import  now
+
 from rest_framework import serializers
-from .models import  Task
+from .models import  Task, SubTask, Category
 
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
@@ -11,3 +13,53 @@ class TaskSerializer(serializers.ModelSerializer):
             'status',
             'deadline'
         ]
+
+
+class SubTaskCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SubTask
+        fields = '__all__'
+        read_only_fields = ['created_at']
+
+
+class CategoryCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+    def validate_name(self, value):
+        if Category.objects.filter(name=value).exists():
+            raise serializers.ValidationError('Category already exists')
+        return value
+
+    def create(self, validated_data):
+        return Category.objects.create(**validated_data)
+
+    def update(self, obj, validated_data):
+        if 'name' in validated_data:
+            name = validated_data['name']
+            if Category.objects.exclude(pk=obj.pk).filter(name=name).exists():
+                raise serializers.ValidationError('Category already exists')
+        return super().update(obj, validated_data)
+
+class SubTaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SubTask
+        fields = '__all__'
+
+class TaskDetailSerializer(serializers.ModelSerializer):
+    subtasks = SubTaskSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Task
+        fields = '__all__'
+
+class TaskCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = '__all__'
+
+    def validate_deadline(self, value):
+        if value < now().date():
+            raise serializers.ValidationError('Deadline cannot be before today')
+        return value
--- urls.py
+++ urls.py
@@ -1,9 +1,18 @@
 from django.urls import path
-from task_manager.views import create_task, list_tasks, get_task_by_id, task_statistics
+from task_manager.views import (
+    create_task,
+    list_tasks,
+    get_task_by_id,
+    task_statistics,
+    SubTaskListCreateView,
+    SubTaskDetailUpdateDeleteView,
+)
 
 urlpatterns = [
     path('tasks/create/', create_task, name='task-create'),
     path('tasks/', list_tasks, name='task-list'),
     path('task/<int:task_id>/', get_task_by_id, name='task-detail'),
     path('tasks/statistics/', task_statistics, name='task-statistics'),
+    path('subtasks/', SubTaskListCreateView.as_view(), name='subtask-list-create'),
+    path('subtasks/', SubTaskDetailUpdateDeleteView.as_view(), name='subtask-detail'),
 ]
